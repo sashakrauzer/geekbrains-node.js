a4a242b177d8613a5512a9415128715d8ed140e1
Гусев Александр Александрович <gusevaa1@sovcombank.ru>
2017-03-09 11:13:18 +0300

    update

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('cookie-session');
 const bodyParser = require('body-parser');
+const templating = require('consolidate');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8082;
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.engine('pug', templating.pug);
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+// const jsonParser = bodyParser.json();
+// const textParser = bodyParser.text({ type: 'text/html' });
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('12345'));
 app.use(session({keys: ['secret']}));
 app.use(passport.initialize());
@@ -31,6 +41,16 @@ passport.use(
     )
 );
 
+passport.serializeUser(function(user, done) {
+    console.log('serialize', user);
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    console.log('deserialize', user);
+    done(null, user);
+});
+
 // const auth = passport.authenticate('local', {
 //     successRedirect: '/user',
 //     failureRedirect: '/login'
@@ -48,13 +68,24 @@ app.all('/user', mustBeAuthenticated);
 app.all('/user/*', mustBeAuthenticated);
 
 app.get('/login', (req, res) => {
-    res.send('LOGIN FORM');
+    res.render('login-form');
+});
+
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
 });
 
 app.post('/login', passport.authenticate('local', { successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true }
+    failureRedirect: '/login'}
     ));
+
+// app.post('/login', (req, res) => {
+    
+//     console.log(req.body);
+//     // res.json(req.body);
+//     res.send(req.body.username);
+// });
 
 // app.use((req, res, next) => {
 //     if(req.url !== '/favicon.ico' && req.url !== '/humans.txt' && req.url !== '/robots.txt') {
